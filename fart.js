d9c3b03d479f13c2aa29954b0cca75016d2c723d
Steven Antonsen <stevenantonsen@Stevens-MacBook-Pro.local>
2019-04-03 03:48:52 -0700

    fart

---
+++ fart.js
@@ -0,0 +1,12 @@
+
+
+function randomFarts(){
+
+var farts = ["fart-02.mp3", "fart-03.mp3", "fart-04.mp3", "fart-05.mp3", "fart-06.mp3", "fart-07.mp3", "fart-08.mp3", "fart-squeak-01.mp3", "fart-squeak-02.mp3", "fart-squeak-03.mp3"];
+
+
+
+}
+
+
+
